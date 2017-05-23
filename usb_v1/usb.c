4e9325f331845c5929710a0ee8b20ca3184af60e
malongfei@zettakit.com <malongfei@zettakit.com>
2017-05-23 11:45:48 +0800

    Add function to get bInterfaceClass
    Add function to get bInterfaceClass, which stands for different
    kind of usb type.
    
            LIBUSB_CLASS_PER_INTERFACE = 0,
    
            /** Audio class */
            LIBUSB_CLASS_AUDIO = 1,
    
            /** Communications class */
            LIBUSB_CLASS_COMM = 2,
    
            /** Human Interface Device class */
            LIBUSB_CLASS_HID = 3,
    
            /** Physical */
            LIBUSB_CLASS_PHYSICAL = 5,
    
            /** Printer class */
            LIBUSB_CLASS_PRINTER = 7,
    
            /** Image class */
            LIBUSB_CLASS_PTP = 6, /* legacy name from libusb-0.1 usb.h */
            LIBUSB_CLASS_IMAGE = 6,
    
            /** Mass storage class */
            LIBUSB_CLASS_MASS_STORAGE = 8,
    
            /** Hub class */
            LIBUSB_CLASS_HUB = 9,
    
            /** Data class */
            LIBUSB_CLASS_DATA = 10,
    
            /** Smart Card */
            LIBUSB_CLASS_SMART_CARD = 0x0b,
    
            /** Content Security */
            LIBUSB_CLASS_CONTENT_SECURITY = 0x0d,
    
            /** Video */
            LIBUSB_CLASS_VIDEO = 0x0e,
    
            /** Personal Healthcare */
            LIBUSB_CLASS_PERSONAL_HEALTHCARE = 0x0f,
    
            /** Diagnostic Device */
            LIBUSB_CLASS_DIAGNOSTIC_DEVICE = 0xdc,
    
            /** Wireless class */
            LIBUSB_CLASS_WIRELESS = 0xe0,
    
            /** Application class */
            LIBUSB_CLASS_APPLICATION = 0xfe,
    
            /** Class is vendor-specific */
            LIBUSB_CLASS_VENDOR_SPEC = 0xff
    
    Signed-off-by: malongfei@zettakit.com <malongfei@zettakit.com>

--- usb_v1/usb.c
+++ usb_v1/usb.c
@@ -4,6 +4,25 @@
 
 char enp_num[8], buf8[8], is_change = 0;
 
+void get_interface_class(libusb_device *dev, uint8_t *interface_class)
+{
+    int i, j;
+    struct libusb_config_descriptor *config;
+    struct libusb_interface *interface;
+    struct libusb_interface_descriptor *altsetting;
+
+    int r = libusb_get_config_descriptor(dev, 0, &config);
+    if (r != 0 ) {
+        printf("failed to get config descriptor.\n");
+        return;
+    }
+
+    for( i = 0; i < config->bNumInterfaces; i++) {
+        *interface_class = config->interface[i].altsetting->bInterfaceClass;
+    }
+
+    libusb_free_config_descriptor(config);
+}
 
 void get_vid_pid_and_bcd_device(libusb_device *dev, uint16_t *vid, uint16_t *pid, uint16_t *bcd_device)
 {
@@ -37,6 +56,7 @@ int main(int argc, char *argv[])
     uint16_t vid = -1;
     uint16_t pid = -1;
     uint16_t bcd_device = -1;
+    uint8_t interface_class = -1;
 
     r = libusb_init(&ctx);
     if (r < 0)
@@ -49,13 +69,14 @@ int main(int argc, char *argv[])
     if (dev_count < 0 )
         printf("get devices count failed.\n");
     else
-        printf("There are %d usb devices on this machine.\n", dev_count);
+        printf("There are %d usb devices on this machine.\n\n", dev_count);
 
     for (i = 0;  i < dev_count; i++) {
         bus_num  = libusb_get_bus_number(devs[i]);
         address  = libusb_get_device_address(devs[i]);
         get_vid_pid_and_bcd_device(devs[i], &vid, &pid, &bcd_device);
-        printf("BusId:%d, DeviceId: %d, %04x:%04x--%04x.\n", bus_num, address, vid, pid, bcd_device);
+        get_interface_class(devs[i], &interface_class);
+        printf("InterfaceClass: %d, %d-%d, %04x:%04x--%04x.\n", interface_class, bus_num, address, vid, pid, bcd_device);
     }
     
     return 0;
